2fdc6070304d6ca2506278dca3f3a6f40d9f4bcd
TheCreeperZenior <thecreeperzenior@gmail.com>
2024-05-20 02:30:28 -0400

    refactor: changed hash validation from previous imports

--- file/import_from_files.py
+++ file/import_from_files.py
@@ -1,18 +1,27 @@
 import hashlib
 import multiprocessing
+from bson import objectid
 from file.read_file import READ_FILE
-from database.get_database import GET_DATABASE
+from utils.search import SEARCH_HASH
+from utils.types import TypesLoadedData, TypesInsertedData
 from database.write_player_to_database import WRITE_PLAYER_TO_DATABASE
 
 
 def IMPORT_FROM_FILES(
-    config: dict[str, str],
     list_logs_files: list[str],
     proxy_type: str,
     directory: str,
     progress_queue: multiprocessing.Queue,
+    loadedData: TypesLoadedData,
 ):
-    db = GET_DATABASE(config["mongodb_connection_string"])
+    insertData: TypesInsertedData = {
+        "player": [],
+        "ip_address": [],
+        "player_ip": [],
+        "file": [],
+        "activity": [],
+    }
+
     for index, log_file in enumerate(list_logs_files):
         # print("[Queue " + str(queue_number) + "] " + log_file)
         file_lines = READ_FILE(directory + "\\" + log_file)
@@ -25,32 +34,33 @@ def IMPORT_FROM_FILES(
         # cuando es ==-1 significa que el hash no está presente.
         # Lo ideal es ignorar los archivos llamados latest.log porque todavia no
         # se manejan bien.
-        if log_file != "latest.log":
-            log_file_result = db["file"].update_one(
-                {"hash": file_hash},
-                {
-                    "$set": {
-                        "filename": log_file,
-                        "proxy_type": proxy_type,
-                        "hash": file_hash,
-                    }
-                },
-                upsert=True,
-            )
-            log_file_id = log_file_result.upserted_id
+        fileIsLoaded = SEARCH_HASH(file_hash, loadedData["file"])
 
-            if log_file_id is not None:
-                # El archivo es nuevo y se insertó en la db
+        if log_file != "latest.log":
+            if fileIsLoaded != -1:
+                # Creación del objeto
+                log_file_id = objectid.ObjectId()
+                insertData["file"].append(
+                    {
+                        "_id": log_file_id,
+                        "file_name": log_file,
+                        "hash": file_hash,
+                        "proxy_type": proxy_type,
+                    }
+                )
+
                 for merged_line in file_lines:
                     WRITE_PLAYER_TO_DATABASE(
-                        db,
                         merged_line,
                         log_file,
                         log_file_id,
+                        insertData,
+                        loadedData,
                     )
             else:
                 print("skipped file " + log_file + " as it was already loaded.")
         else:
             print("ignored latest.log")
 
-        progress_queue.put(1)
+        progress_queue.put((1, {}))
+    progress_queue.put((100, insertData))
